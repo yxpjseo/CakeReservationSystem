d26fc39ec9e7c34ccb9e8be2031157abbf64dc4e
seonhyeong205 <tosssara@ewhain.net>
2025-06-05 19:23:33 +0900

    Update select_popular_cakes_by_size.sql

--- src/main/resources/sql/operation/select_popular_cakes_by_size.sql
+++ src/main/resources/sql/operation/select_popular_cakes_by_size.sql
@@ -1,6 +1,7 @@
--- VIEW cakes_total_ordered 이용
-SELECT cake_name, total_ordered
-FROM cakes_total_ordered
-WHERE size = ?
+SELECT c.cake_name, SUM(oi.count) AS total_ordered
+FROM cakes c
+JOIN order_items oi ON c.cake_name = oi.cake_name
+WHERE c.size = ?
+GROUP BY c.cake_name
 ORDER BY total_ordered DESC
 LIMIT 5;
